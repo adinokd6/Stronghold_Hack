018c75863f616e89464d1d731da39e536dfdab17
adinokd6 <adinokd6@gmail.com>
2021-03-15 20:28:27 +0100

    Added static text fields

--- Stronghold/cMain.cpp
+++ Stronghold/cMain.cpp
@@ -1,14 +1,39 @@
 #include "cMain.h"
+#include <string>
 
 
-cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Stronghold Fast Hack", wxPoint(30,30), wxSize(720x300)
+//wxMessageBox();
+
+wxBEGIN_EVENT_TABLE(cMain,wxFrame)
+wxEND_EVENT_TABLE()
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Stronghold Fast Hack", wxPoint(30,30), wxSize(325,720))
 {
+	hack_button = new wxButton(this,10001, "HACK",wxPoint(10,10),wxSize(290,50));
+	typing_fields = new wxTextCtrl*[numbers_of_fields];
+	actual_value = new wxStaticText*[numbers_of_fields];
+	pictures = new wxImage * [];
+	for (int i = 0;i < numbers_of_fields;i++)
+	{
+		typing_fields[i] = new wxTextCtrl(this, wxID_ANY, "", wxPoint(120, 70+i*35), wxSize(50, 20));
+		actual_value[i] = new wxStaticText(this, wxID_ANY, "1500", wxPoint(70, 70 + i * 35), wxSize(50, 20));
+		pictures[i] = new wxImage();
+		std::string s = std::to_string(i % 2);
+		s.append(".png");
+		pictures[i]->LoadFile(wxT(s), wxBITMAP_TYPE_PNG);
+	}
 	
 
 }
 
 cMain::~cMain()
 {
+	delete[]typing_fields;
+	delete[]actual_value;
+	delete[]pictures;
+}
 
-
+void cMain::OnButtonClicked(wxCommandEvent& evnt)
+{
+	
 }
--- Stronghold/cMain.h
+++ Stronghold/cMain.h
@@ -1,11 +1,22 @@
 #pragma once
 
 #include "wx/wx.h"
+#include <string>
+
+
 
 class cMain : public wxFrame
 {
 public: 
 	cMain();
 	~cMain();
+	void OnButtonClicked(wxCommandEvent& evnt);
+	wxDECLARE_EVENT_TABLE();
+private:
+	int numbers_of_fields = 8;
+	wxButton *hack_button = nullptr;
+	wxTextCtrl **typing_fields;
+	wxStaticText **actual_value;
+	wxImage **pictures;
 };
 
